018b1526dcf91711f68813c1871d15fb0dbbd67d
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2023-09-21 08:04:21 -0300

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -16,3 +16,22 @@ imagens.forEach((item, index) =>{
         ativarConteudo(index)
     })
 })
+
+////////////////////////////////////////////////
+
+const faq = document.querySelectorAll('.js-faq dt')
+const faq2 = document.querySelectorAll('.js-faq dd')
+
+console.log(faq)
+console.log(faq2)
+
+function ativarConteudo2(indice){
+    faq2[indice].classList.toggle('active')
+    faq[indice].classList.toggle('active')
+}
+
+faq.forEach((item, index) =>{
+    item.addEventListener('click', () => {
+        ativarConteudo2(index)
+    })
+})
